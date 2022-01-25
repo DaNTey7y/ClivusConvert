903b776df4b123cc2303d15cfd0bcbf2fbfa1d5c
DaNTey7y <xvhuqdph@gmail.com>
2022-01-25 18:23:21 +0500

    это база

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -7,8 +7,15 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 @app.route('/home', methods=['GET'])
 def home():
-    return '<h1>suck</h1>'
+    return render_template('home.html', title='База')
+
+
+def main():
+    # app.config['SECRET_KEY'] = че нибудь сюда хуйнуть рандомную строку типа
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(host='0.0.0.0', port=port)
+    app.run(port=8080, host='127.0.0.1')
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    main()
