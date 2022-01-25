30cd87e91db3f756fa4f6b64cc9191123cb1a0d8
DaNTey7y <xvhuqdph@gmail.com>
2022-01-25 16:50:33 +0500

    -suck

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('Start')
+from flask import Flask, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+@app.route('/home', methods=['GET'])
+def home():
+    return '<h1>suck</h1>'
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
