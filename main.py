d0d4b914529838640da034ee87943d275524d223
DaNTey7y <xvhuqdph@gmail.com>
2022-01-31 18:56:41 +0500

    добавил переход в опции

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from functions.generate_random_string import generate_random_string
 from random import choice
 
@@ -6,14 +6,41 @@ from random import choice
 app = Flask(__name__)
 
 
+playlist = ['static/audio/shit.mp3', 'static/audio/teplovizor.mp3',
+            'static/audio/govno.mp3', 'static/audio/banger.mp3',
+            'static/audio/shit2.mp3', 'static/audio/shit3.mp3',
+            'static/audio/cock.mp3', 'static/audio/shedevr.mp3']
+
+
 @app.route('/', methods=['GET'])
 @app.route('/home', methods=['GET'])
 def home():
-    playlist = ['static/audio/shit.mp3', 'static/audio/banger.mp3',
-                'static/audio/govno.mp3', 'static/audio/shedevr.mp3',
-                'static/audio/shit2.mp3', 'static/audio/shit3.mp3']
-    music = choice(playlist)
-    return render_template('home.html', title='База', music=music)
+    params = dict()
+    params['title'] = 'База'
+    params['music'] = choice(playlist)
+    return render_template('home.html', **params)
+
+
+@app.route('/text-to-gradient', methods=['GET', 'POST'])
+def text_to_gradient():
+    if request.method == 'GET':
+        params = dict()
+        params['title'] = 'Сучий градиент'
+        params['music'] = choice(playlist)
+        return render_template('text-to-gradient.html', **params)
+    elif request.method == 'POST':
+        return '123'
+
+
+@app.route('/image-to-audio', methods=['GET', 'POST'])
+def image_to_audio():
+    if request.method == 'GET':
+        params = dict()
+        params['title'] = 'Тайлер Дёрден'
+        params['music'] = choice(playlist)
+        return render_template('text-to-gradient.html', **params)
+    elif request.method == 'POST':
+        return 'Мы вообще будем это делать?'
 
 
 def main():
